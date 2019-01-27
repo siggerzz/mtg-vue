b3b059cdbdf530fca4c0652ade1d1a065d7db52a
Ben Siggery <ben.siggery@just-eat.com>
2019-01-27 18:21:37 +0000

    Move component to search

--- src/components/SearchComponent.vue
+++ src/components/SearchComponent.vue
@@ -5,23 +5,73 @@
       <SearchCardComponent class="search-card-component"/>
       <SearchColourComponent class="search-colour-component"/>
   </div>
-    <FilterByComponent class="filter-by-component"/>
+    <div class="filter-by-container">
+    <h3>Filter by:</h3>
+    <div class="filter-select select-style">
+        <p>Card Format:</p>
+        <select>
+            <option value=""></option>
+            <option value="test">Test</option>
+        </select>
+    </div>
+        <div class="filter-select select-style">
+        <p>Card Set:</p>
+        <select>
+            <option value=""></option>
+            <option value="test" v-for="(set, index) in setsResponse.sets" :key=index>{{set.name}}</option>
+        </select>
+    </div>
+        <div class="filter-select select-style">
+        <p>Card Type:</p>
+        <select>
+            <option value=""></option>
+            <option value="test">Test</option>
+        </select>
+    </div>
+        <div class="filter-select select-style">
+        <p>Select Sort Order:</p>
+        <select>
+            <option value=""></option>
+            <option value="test">Test</option>
+        </select>
+    </div>
+        <div class="filter-select select-style">
+        <p>Select Output Format:</p>
+        <select>
+            <option value=""></option>
+            <option value="test">Test</option>
+        </select>
+    </div>
+    </div>
 </div>
 </template>
 
 <script>
+import { RepositoryFactory } from '../api/repositories/RepositoryFactory';
 import SearchBoxComponent from './search/SearchBoxComponent.vue';
 import SearchCardComponent from './search/SearchCardComponent.vue';
 import SearchColourComponent from './search/SearchColourComponent.vue';
-import FilterByComponent from './search/FilterByComponent.vue';
+
+const SetsRepository = RepositoryFactory.get('sets');
 
 export default {
   name: 'SearchComponent',
   components: {
     SearchBoxComponent,
     SearchCardComponent,
-    SearchColourComponent,
-    FilterByComponent
+    SearchColourComponent
+  },
+  data() {
+    return {
+      setsResponse: {}
+  }},
+  methods: {
+      async fetchSets() {
+      await SetsRepository.getSets().then(response => {
+      this.setsResponse = (response.data);
+  })}},
+  created() {
+    this.fetchSets();
   }
 }
 </script>
@@ -29,11 +79,52 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
 
+
+
+
+
+//Filter By Component
+h3 {
+    text-align: left;
+}
+.filter-select {
+    text-align: left;
+}
+
+
+.select-style {
+    border-radius: 3px;
+    overflow: hidden;
+}
+
+.select-style select {
+    width:100%;
+    height: 31px;
+    border: none;
+    box-shadow: none;
+    color: #ffffff;
+    background-color: #232323;
+    -webkit-appearance: none;
+}
+
+.select-style select:focus {
+    outline: none;
+}
+
 @media screen and (max-width: 750px) {
     .search-by-container {
     width: 90%;
     margin: 0 auto;
     display: block;
+    text-align: center;
+  }
+
+  .filter-by-container {
+    width:90%;
+    margin:0 auto;
+  }
+
+  .filter-by-container h3, p {
     text-align: center;
   }
 }
--- src/components/search/FilterByComponent.vue
+++ src/components/search/FilterByComponent.vue
@@ -1,114 +1,14 @@
 <template>
-    <div class="filter-by-container">
-    <h3>Filter by:</h3>
-    <div class="filter-select select-style">
-        <p>Card Format:</p>
-        <select>
-            <option value=""></option>
-            <option value="test">Test</option>
-        </select>
-    </div>
-        <div class="filter-select select-style">
-        <p>Card Set:</p>
-        <select>
-            <option value=""></option>
-            <option value="test" v-for="(set, index) in setsResponse.sets" :key=index>{{set.name}}</option>
-        </select>
-    </div>
-        <div class="filter-select select-style">
-        <p>Card Type:</p>
-        <select>
-            <option value=""></option>
-            <option value="test">Test</option>
-        </select>
-    </div>
-        <div class="filter-select select-style">
-        <p>Select Sort Order:</p>
-        <select>
-            <option value=""></option>
-            <option value="test">Test</option>
-        </select>
-    </div>
-        <div class="filter-select select-style">
-        <p>Select Output Format:</p>
-        <select>
-            <option value=""></option>
-            <option value="test">Test</option>
-        </select>
-    </div>
-    </div>
 </template>
 
-<script>
-import { RepositoryFactory } from '../../api/repositories/RepositoryFactory';
-const SetsRepository = RepositoryFactory.get('sets');
-
-export default {
-    name:"FilterByComponent",
-    data() {
-        return {
-            setsResponse: {}
-        }
-    },
-    methods: {
-        async fetchSets() {
-        await SetsRepository.getSets().then(response => {
-        this.setsResponse = (response.data);
-    })}},
-    created() {
-    this.fetchSets();
-  }
-}
-</script>
 
 <style lang="scss" scoped>
 
-h3 {
-    text-align: left;
-}
-.filter-select {
-    text-align: left;
-}
-
-
-.select-style {
-    border-radius: 3px;
-    overflow: hidden;
-}
-
-.select-style select {
-    width:100%;
-    height: 31px;
-    border: none;
-    box-shadow: none;
-    color: #ffffff;
-    background-color: #232323;
-    -webkit-appearance: none;
-}
-
-.select-style select:focus {
-    outline: none;
-}
 
 @media screen and (min-width: 750px) {
-    .filter-by-container {
-        width:100%;
-    }
-
-    .filter-by-container {
-        width:90%;
-    }
 }
 
 @media screen and (max-width: 750px) {
-    .filter-by-container {
-        width:90%;
-        margin:0 auto;
-    }
-
-    .filter-by-container h3, p {
-        text-align: center;
-    }
 }
 </style>
 
