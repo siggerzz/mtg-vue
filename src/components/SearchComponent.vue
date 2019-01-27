a347ad0e80867a17463dd810bde281ab700a9211
Ben Siggery <ben.siggery@just-eat.com>
2019-01-27 18:26:41 +0000

    Move to search component.

--- src/components/SearchComponent.vue
+++ src/components/SearchComponent.vue
@@ -3,7 +3,48 @@
   <SearchBoxComponent/>
   <div class="search-by-container">
       <SearchCardComponent class="search-card-component"/>
-      <SearchColourComponent class="search-colour-component"/>
+      <div class="search-colour">
+   <div class="search-colour-header">
+      <h3>Search by Colour:</h3>
+   </div>
+   <label class="checkbox-container">
+        <img src="../assets/card-colours/white.png">
+        <input v-model="checkedColors" name="color" value="white" type="checkbox">
+        <span class="checkmark"></span>
+   </label>
+        <label class="checkbox-container">
+        <img src="../assets/card-colours/blue.png">
+        <input v-model="checkedColors" name="color" value="blue" type="checkbox">
+        <span class="checkmark"></span>
+   </label>
+   <label class="checkbox-container">
+        <img src="../assets/card-colours/black.png">
+        <input v-model="checkedColors" name="color" value="black" type="checkbox">
+        <span class="checkmark"></span>
+   </label>
+   <label class="checkbox-container">
+        <img src="../assets/card-colours/red.png">
+        <input v-model="checkedColors" name="color" value="red" type="checkbox">
+        <span class="checkmark"></span>
+   </label>
+   <label class="checkbox-container">
+        <img src="../assets/card-colours/green.png">
+        <input v-model="checkedColors" name="color" value="green" type="checkbox">
+        <span class="checkmark"></span>
+   </label>
+   <label class="checkbox-container">Match Colors Exactly (use 'and' instead of 'or')
+        <input name="text" type="checkbox">
+        <span class="checkmark"></span>
+   </label>
+   <label class="checkbox-container">Exclude Unselected Colors (limit by selection)
+        <input name="text" type="checkbox">
+        <span class="checkmark"></span>
+   </label>
+   <label class="checkbox-container">Exclude Unselected Colors (limit by selection)
+        <input name="text" type="checkbox">
+        <span class="checkmark"></span>
+   </label>
+</div>
   </div>
     <div class="filter-by-container">
     <h3>Filter by:</h3>
@@ -50,7 +91,6 @@
 import { RepositoryFactory } from '../api/repositories/RepositoryFactory';
 import SearchBoxComponent from './search/SearchBoxComponent.vue';
 import SearchCardComponent from './search/SearchCardComponent.vue';
-import SearchColourComponent from './search/SearchColourComponent.vue';
 
 const SetsRepository = RepositoryFactory.get('sets');
 
@@ -58,11 +98,11 @@ export default {
   name: 'SearchComponent',
   components: {
     SearchBoxComponent,
-    SearchCardComponent,
-    SearchColourComponent
+    SearchCardComponent
   },
   data() {
     return {
+      checkedColors: [],
       setsResponse: {}
   }},
   methods: {
@@ -81,7 +121,23 @@ export default {
 
 
 
+//Search By Colour Component
+.colour-type h3 {
+    display:block;
+}
 
+.colour-type {
+    display:inline-block;
+    padding-left:20px;
+}
+
+.colour-type input {
+    padding:12px;
+}
+
+.search-colour .first {
+    padding-left:0px;
+}
 
 //Filter By Component
 h3 {
@@ -112,11 +168,15 @@ h3 {
 }
 
 @media screen and (max-width: 750px) {
-    .search-by-container {
+  .search-by-container {
     width: 90%;
     margin: 0 auto;
     display: block;
     text-align: center;
+  }
+
+  .search-colour-header h3 {
+    margin-bottom: 0px;
   }
 
   .filter-by-container {
